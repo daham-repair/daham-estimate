2c96d4239176d33034e74fcb0280f56ac522c418
daham-repair <kimjaeyoung8747@gmail.com>
2026-01-30 21:42:50 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
-/* [data.js Ver 1.30] */
-const SYSTEM_VERSION = "Ver 1.31";
+/* [data.js Ver 1.32] */
+const SYSTEM_VERSION = "Ver 1.32";
 
 document.addEventListener('DOMContentLoaded', function() {
     const badge = document.createElement('div');
@@ -40,4 +40,3 @@ const data = [
     { key: 'etc', category: '기타', items: [{n:'입주 청소 (평)', p:15000}, {n:'엘리베이터 보양', p:150000}, {n:'폐기물 처리 (1톤)', p:400000}, {n:'운반비', p:200000}, {n:'공과잡비', p:0}] },
     { key: 'custom', category: '직접 입력', items: [] }
 ];
-
